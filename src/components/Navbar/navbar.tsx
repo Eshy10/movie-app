352fe5dacd41c742f80986904f994bb40f5dc7c3
Eshy10 <adewalebose15@gmail.com>
2022-12-10 11:14:26 +0100

    chore: set up basic files

---
+++ src/components/Navbar/navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div>
+            <h1>My Test App</h1>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar;
---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <section>
+    </section>
+  )
+}
+
+export default Homepage
